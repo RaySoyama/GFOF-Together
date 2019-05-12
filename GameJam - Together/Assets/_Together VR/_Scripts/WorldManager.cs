481e73a34bd3d925f27d0c32e8d118233e6ff600
Ray Soyama <raysoyama@gmail.com>
2019-05-12 13:45:40 -0700

    fixed the reference issue with the animator

--- GameJam - Together/Assets/_Together VR/_Scripts/WorldManager.cs
+++ GameJam - Together/Assets/_Together VR/_Scripts/WorldManager.cs
@@ -98,7 +98,7 @@ public class WorldManager : MonoBehaviour
                 if (Input.GetKeyUp(KeyCode.P))
                 {
                     StartCoroutine(InitializeScene());
-                }
+                }   
 
                 break;
 
