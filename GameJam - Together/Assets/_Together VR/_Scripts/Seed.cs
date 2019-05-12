1e48a881a216a3ff4acd7058214a28cca96090c6
diammen <marcelnamdao@gmail.com>
2019-05-11 20:57:58 -0700

    add seed grabbing detection

---
+++ GameJam - Together/Assets/_Together VR/_Scripts/Seed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seed : MonoBehaviour
+{
+    public WorldManager wm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.parent != null)
+        {
+            Debug.Log("grabbed");
+        }
+    }
+}
