b7f8dd00af613c0b0d05907ca393a3a9f6f5a4ac
diammen <marcelnamdao@gmail.com>
2019-05-11 23:45:39 -0700

    add water to watering can

---
+++ GameJam - Together/Assets/_Together VR/_Scripts/WateringCan.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WateringCan : MonoBehaviour
+{
+
+    public ParticleSystem water;
+    ParticleSystem.EmissionModule emissionModule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        emissionModule = water.emission;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        emissionModule.rateOverTime = Mathf.Lerp(0, 25, 1);
+    }
+}
